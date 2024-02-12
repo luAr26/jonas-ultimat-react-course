9f49c3f108b08ab698e3a8500d4f3a5d22e01ab2
luAr26 <raul_cosmin_26@yahoo.com>
2024-02-12 19:59:50 +0200

    Random commit.

--- 05-travel-list/travel-list/src/components/Form.jsx
+++ 05-travel-list/travel-list/src/components/Form.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    setDescription("");
+    setQuantity(1);
+    console.log(newItem);
   }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="" id="">
+      <select
+        name="quantity"
+        id="quantity"
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        id="description"
+        name="description"
+      />
       <button>Add</button>
     </form>
   );
