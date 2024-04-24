e9e1636c772c2ff76440c7bdcceca38600630b03
luAr26 <raul_cosmin_26@yahoo.com>
2024-04-24 21:46:23 +0300

    Learn the basics of Redux.

---
+++ 14-redux-in-isolation/src/store.js
@@ -0,0 +1,31 @@
+/** @format */
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: state.loan + action.payload,
+        loanPurpose: action.purpose,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
