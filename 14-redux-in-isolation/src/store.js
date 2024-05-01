59765407dcab23b755cb576703732a6ca1dfacaf
Raul <raul_cosmin_26@yahoo.com>
2024-05-01 21:42:14 +0300

    Complete the legacy redux part.

--- 14-redux-in-isolation/src/store.js
+++ 14-redux-in-isolation/src/store.js
@@ -1,14 +1,23 @@
 /** @format */
-import { combineReducers, legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 // import { createCustomer } from "./features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 // store.dispatch(createCustomer({ fullName: "Raul S.", nationalID: "6730" }));
 
 // console.log(store.getState());
