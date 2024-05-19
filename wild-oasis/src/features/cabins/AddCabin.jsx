5248b39a27507a46d1d3c77f8bf028fedd369eee
Raul <raul_cosmin_26@yahoo.com>
2024-05-19 20:54:37 +0300

    Add confirm cabin deletions.

--- wild-oasis/src/features/cabins/AddCabin.jsx
+++ wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,7 +1,5 @@
 /** @format */
 
-// import { useState } from "react";
-
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CabinTable from "./CabinTable";
@@ -9,42 +7,23 @@ import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens='cabin-form'>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name='cabin-form'>
-        <CreateCabinForm />
-      </Modal.Window>
-      <Modal.Open opens='table'>
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name='table'>
-        <CabinTable />
-      </Modal.Window>
-    </Modal>
+    <div>
+      <Modal>
+        <Modal.Open opens='cabin-form'>
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name='cabin-form'>
+          <CreateCabinForm />
+        </Modal.Window>
+        <Modal.Open opens='table'>
+          <Button>Show table</Button>
+        </Modal.Open>
+        <Modal.Window name='table'>
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
+    </div>
   );
 }
-
-// function AddCabin() {
-//   const [isOpenModal, setIsOpenModal] = useState(false);
-//   const handleClose = () => setIsOpenModal(false);
-//   return (
-//     <>
-//       <Button
-//         onClick={() => setIsOpenModal((prev) => !prev)}
-//         $variation='secondary'
-//         $size='large'
-//       >
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={handleClose}>
-//           <CreateCabinForm onCloseModal={handleClose} />
-//         </Modal>
-//       )}
-//     </>
-//   );
-// }
 
 export default AddCabin;
