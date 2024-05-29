8257f83e931d83cdc7319225711adb689676b865
Raul <raul_cosmin_26@yahoo.com>
2024-05-29 21:14:35 +0300

    Add API-Side Sorting: Sort Bookings.

--- wild-oasis/src/services/apiBookings.js
+++ wild-oasis/src/services/apiBookings.js
@@ -12,10 +12,17 @@ export async function getBookings({ filter, sortBy }) {
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
     );
 
-  if (filter !== null) {
+  // FILTER
+  if (filter) {
     query = query[filter.method || "eq"](filter.field, filter.value);
   }
 
+  // SORT
+  if (sortBy) {
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
+  }
   const { data, error } = await query;
 
   if (error) {
