c489a2b37fb70441dbd0b3fc6805ab0b4b5dc8a9
Raul <raul_cosmin_26@yahoo.com>
2024-06-09 20:31:44 +0300

    Compute the recent bookings and stays.

--- wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,9 @@
 /** @format */
 
 import styled from "styled-components";
+import Spinner from "./../../ui/Spinner";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -10,7 +13,23 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
-  return <StyledDashboardLayout />;
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoadingStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  console.log(bookings);
+  console.log(stays);
+  console.log(confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today&apos;s activity</div>
+      <div>Chart for stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
 }
 
 export default DashboardLayout;
