d056904ea1bab7d59720b0eb91dfee9937f79ff9
Raul <raul_cosmin_26@yahoo.com>
2024-06-20 20:57:19 +0300

    Fetching data in a page.

--- the-wild-oasis-website/app/cabins/page.js
+++ the-wild-oasis-website/app/cabins/page.js
@@ -1,4 +1,16 @@
 /** @format */
-export default function Page() {
-  return <h1>Cabins page</h1>;
+export default async function Page() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersJson = await users.json();
+  console.log(usersJson);
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {usersJson.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
