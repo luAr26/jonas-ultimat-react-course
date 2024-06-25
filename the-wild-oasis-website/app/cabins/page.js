d5b9d8f1c210c1e8ce6fd71be82e04d7dc726fa9
Raul <raul_cosmin_26@yahoo.com>
2024-06-25 21:23:52 +0300

    The wild oasis website - project organization.

--- the-wild-oasis-website/app/cabins/page.js
+++ the-wild-oasis-website/app/cabins/page.js
@@ -1,18 +1,8 @@
 /** @format */
-import Counter from "../components/Counter";
-export default async function Page() {
-  const users = await fetch("https://jsonplaceholder.typicode.com/users");
-  const usersJson = await users.json();
-  console.log(usersJson);
-
+export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <h3>Counter</h3>
-      {usersJson.map((user) => (
-        <p key={user.id}>{user.website}</p>
-      ))}
-      <Counter users={usersJson} />
     </div>
   );
 }
