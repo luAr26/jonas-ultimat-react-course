c82f8ab2d0c83c850ede62c7efc48263540aada1
Raul <raul_cosmin_26@yahoo.com>
2024-06-29 22:04:46 +0300

    Complete the website setup.

--- the-wild-oasis-website/app/account/page.js
+++ the-wild-oasis-website/app/account/page.js
@@ -5,5 +5,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Account page</h1>;
+  return (
+    <h2 className='text-2xl font-semibold text-accent-400 mb-7'>
+      Welcome, Jonas
+    </h2>
+  );
 }
