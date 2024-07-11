b77e524717acf7c02cca438044e92ee15df23345
Raul <raul_cosmin_26@yahoo.com>
2024-07-11 21:22:56 +0300

    Create an API Endpoint with route handlers.

---
+++ the-wild-oasis-website/app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(__, { params }) {
+  const { cabinId } = params;
+  console.log(cabinId);
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+    return Response.json({ cabin, bookedDates });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found." });
+  }
+}
+// export async function POST() {}
