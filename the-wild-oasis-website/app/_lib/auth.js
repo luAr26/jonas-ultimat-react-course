bc3f104cbbe397df2d27e88c55856cb73d96ff21
Raul <raul_cosmin_26@yahoo.com>
2024-07-12 20:49:56 +0300

    Make next auth work.

--- the-wild-oasis-website/app/_lib/auth.js
+++ the-wild-oasis-website/app/_lib/auth.js
@@ -3,16 +3,17 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+// const authConfig = {
+//   providers: [
+//     Google({
+//       clientId: process.env.AUTH_GOOGLE_ID,
+//       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+//     }),
+//   ],
+// };
+
 const authConfig = {
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+  providers: [Google],
 };
 
-export const {
-  auth,
-  handlers: { GET, POST },
-} = NextAuth(authConfig);
+export const { auth, handlers } = NextAuth(authConfig);
--- the-wild-oasis-website/app/auth/[...nextauth]/route.js
+++ the-wild-oasis-website/app/auth/[...nextauth]/route.js
@@ -1,3 +1,3 @@
 /** @format */
-
-export { GET, POST } from "@/app/_lib/auth";
+import { handlers } from "@/app/_lib/auth";
+export const { GET, POST } = handlers;
