095f8ec57c326a2fca21af3003b6a99ee651ce27
Raul <raul_cosmin_26@yahoo.com>
2024-07-14 10:47:12 +0300

    Delete a reservation.

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -31,6 +31,26 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session)
+    throw new Error("You must be signed in to delete a reservation");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You do not have permission to delete this reservation");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Reservation could not be deleted");
+  revalidatePath("/account/reservations");
+}
+
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
