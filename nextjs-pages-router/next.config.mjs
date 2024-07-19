f210893bba35b285a43650beef26cb0e5e56f50a
Raul <raul_cosmin_26@yahoo.com>
2024-07-19 20:47:17 +0300

    Setup a pages router nextjs project.

---
+++ nextjs-pages-router/next.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wunilooqeqtriufpoqvw.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
---
+++ nextjs-pages-router/pages/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+function Home() {
+  return <h1 className='text-3xl'>Hello Pages Router</h1>;
+}
+
+export default Home;
